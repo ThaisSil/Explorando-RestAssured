2726407d8aa658eabc410670186144dfb6b81800
ThaisSil <thatsi.fer@gmail.com>
2023-01-27 17:27:31 -0300

    adicionando mudanças

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -3,68 +3,99 @@ import Entities.BookingDates;
 import Entities.User;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
-import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.LogConfig.logConfig;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BookingTests {
-    public static String token = "";
     public static Faker faker;
     private static RequestSpecification request;
     private static Booking booking;
     private static BookingDates bookingDates;
     private static User user;
+    private Object token;
 
     @BeforeAll
-    public static void Setup(){
+    public static void Setup() {
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         faker = new Faker();
         user = new User(faker.name().username(),
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.internet().safeEmailAddress(),
-                faker.internet().password(8,10),
+                faker.internet().password(8, 10),
                 faker.phoneNumber().toString());
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        booking = new Booking(user.getFirstname(), user.getLastname(),
-                (float)faker.number().randomDouble(2,50,100000),
-                true,bookingDates,"");
 
+        bookingDates = new BookingDates("2018-01-02", "2018-01-03");
+        booking = new Booking(user.getFirstname(), user.getLastname(),
+                (float) faker.number().randomDouble(2, 50, 100000),
+                true, bookingDates,
+                "");
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new ErrorLoggingFilter());
     }
+
     @BeforeEach
-    void setRequest(){
+    void setRequest() {
         request = given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
                 .contentType(ContentType.JSON)
                 .auth().basic("admin", "password123");
+
     }
 
-    @Test
-    public void getAllBookingByIds_returnOk(){
+    @Test // Create Token
+    public void createAuthToken() {
+        Map<String, String> body = new HashMap<>();
+        body.put("username", "admin");
+        body.put("password", "password123");
+
+        token = request
+                .header("ContentType", "application/json")
+                .contentType(ContentType.JSON)
+                .when()
+                .body(body)
+                .post("/auth")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .path("token");
+    }
+
+
+    @Test // Get BookingIds
+    public void getAllBookingsById_returnOk() {
         Response response = request
                 .when()
                 .get("/booking")
                 .then()
                 .extract()
                 .response();
+
+
         Assertions.assertNotNull(response);
-        Assertions.assertEquals(200, response.statusCode());
+        assertEquals(200, response.statusCode());
     }
-    @Test
-    public void getAllBookingsByUserFirstName_BookingExists_returnOk(){
+
+    @Test // Get Booking
+    public void getAllBookingsByUserFirstName_BookingExists_returnOk() {
         request
                 .when()
-                .queryParam("firstName", faker.name().firstName())
+                .queryParam("firstName", "Carol")
                 .get("/booking")
                 .then()
                 .assertThat()
@@ -76,22 +107,21 @@ public class BookingTests {
     }
 
     @Test
-    public void createBooking_WithValidData_returnOk(){
-            request
-                    .contentType(ContentType.JSON)
-                    .when()
-                    .body(booking)
-                    .post("/booking")
-                    .then()
-                    .body(matchesJsonSchemaInClasspath("createBookingRequestSchema.json"))
-                    .and()
-                    .assertThat()
-                    .statusCode(200)
-                    .contentType(ContentType.JSON).and().time(lessThan(2000L))
-                    .extract()
-                    .jsonPath();
-        }
+
+    public void getAllBookingsByTotalPrice() {
+        request
+                .when()
+                .queryParam("totalprice", faker.number().digits(3))
+                .get("/booking")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .and()
+                .body("results", hasSize(greaterThan(0)));
+    }
 }
+
 
 
 
